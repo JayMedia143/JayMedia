e3597d720a3d0cb5169d4c02e8e29538037fe4fb
JayMedia143 <jaymedia143@gmail.com>
2023-03-01 14:14:13 +0800

    first changes

--- mail.php
+++ mail.php
@@ -1,38 +1,12 @@
 <?php
-                  //first we leave this input field blank
-                  $sender = "";
-                  //if user click the send button
-                  if(isset($_POST['send'])){
-                      //access user entered data
-                     $recipient = "jeremysosa1011@gmail.com";
-                     $message = $_POST['message'];
-                     $sender = $_POST['email'];;
-                     //if user leave empty field among one of them
-                     if(empty($recipient) || empty($message)){
-                         ?>
-               <!-- display an alert message if one of them field is empty -->
-               <div class="alert alert-danger text-center">
-                  <?php echo "All inputs are required!" ?>
-               </div>
-               <?php
-                  }else{
-                      // PHP function to send mail
-                     if(mail($sender, $message, $recipient)){
-                      ?>
-               <!-- display a success message if once mail sent sucessfully -->
-               <div class="alert alert-success text-center">
-                  <?php echo "Your mail successfully sent to $recipient"?>
-               </div>
-               <?php
-                  $sender = "";
-                  }else{
-                   ?>
-               <!-- display an alert message if somehow mail can't be sent -->
-               <div class="alert alert-danger text-center">
-                  <?php echo "Failed while sending your mail!" ?>
-               </div>
-               <?php
-                  }
-                  }
-                  }
-                  ?>
+
+$recepient = "jeremysosa1011@gmail.com";
+$sitename = "Jay Media";
+
+$name = trim($_POST["name"]);
+$email = trim($_POST["email"]);
+$text = trim($_POST["text"]);
+$message = "Name: $name \nEmail: $email \nText: $text";
+
+$pagetitle = "New message from the \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
