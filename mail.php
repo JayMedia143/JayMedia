b9f3f9f6ee234bd08b6aebd2962dac3e6433bc17
JayMedia143 <jaymedia143@gmail.com>
2023-03-01 13:07:04 +0800

    second change

--- mail.php
+++ mail.php
@@ -1,6 +1,6 @@
 <?php
                   //first we leave this input field blank
-                  $recipient = "jeremysosa1011@gmail.com";
+                  $sender = "";
                   //if user click the send button
                   if(isset($_POST['send'])){
                       //access user entered data
@@ -8,7 +8,7 @@
                      $message = $_POST['message'];
                      $sender = $_POST['email'];;
                      //if user leave empty field among one of them
-                     if(empty($sender) || empty($message)){
+                     if(empty($recipient) || empty($message)){
                          ?>
                <!-- display an alert message if one of them field is empty -->
                <div class="alert alert-danger text-center">
@@ -17,14 +17,14 @@
                <?php
                   }else{
                       // PHP function to send mail
-                     if(mail($recipient, $message, $sender)){
+                     if(mail($sender, $message, $recipient)){
                       ?>
                <!-- display a success message if once mail sent sucessfully -->
                <div class="alert alert-success text-center">
                   <?php echo "Your mail successfully sent to $recipient"?>
                </div>
                <?php
-                  $recipient = "jeremysosa1011@gmail.com";
+                  $sender = "";
                   }else{
                    ?>
                <!-- display an alert message if somehow mail can't be sent -->
