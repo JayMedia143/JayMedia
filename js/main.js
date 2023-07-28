6fac0c6edf93b7eb1eccf2e774a95f8873b24c89
JayMedia143 <jaymedia143@gmail.com>
2023-07-28 18:07:14 +0800

    ja

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ p.s. I am available for Freelance hire (UI design, web development). mail: mille
 
 ------------------------------------------- */
 $(function () {
-  "use strict";
+  ("use strict");
 
   // swup js
   const options = {
@@ -106,41 +106,41 @@ $(function () {
 
   // progressbars
 
-  bar.animate(0.8);
+  //  bar.animate(0.8);
 
-  var bar = new ProgressBar.Line(lineprog2, {
-    strokeWidth: 1.72,
-    easing: "easeInOut",
-    duration: 1400,
-    delay: 2900,
-    trailWidth: 1.72,
-    svgStyle: {
-      width: "100%",
-      height: "100%",
-    },
-    step: (state, bar) => {
-      bar.setText(Math.round(bar.value() * 100) + " %");
-    },
-  });
+  // var bar = new ProgressBar.Line(lineprog2, {
+  //   strokeWidth: 1.72,
+  //   easing: "easeInOut",
+  //   duration: 1400,
+  //   delay: 2900,
+  //   trailWidth: 1.72,
+  //    svgStyle: {
+  //     width: "100%",
+  //     height: "100%",
+  //   },
+  //   step: (state, bar) => {
+  //     bar.setText(Math.round(bar.value() * 100) + " %");
+  //   },
+  //  });
 
-  bar.animate(0.65);
+  // bar.animate(0.65);
 
-  var bar = new ProgressBar.Line(lineprog3, {
-    strokeWidth: 1.72,
-    easing: "easeInOut",
-    duration: 1400,
-    delay: 3000,
-    trailWidth: 1.72,
-    svgStyle: {
-      width: "100%",
-      height: "100%",
-    },
-    step: (state, bar) => {
-      bar.setText(Math.round(bar.value() * 100) + " %");
-    },
-  });
+  // var bar = new ProgressBar.Line(lineprog3, {
+  // strokeWidth: 1.72,
+  //  easing: "easeInOut",
+  //  duration: 1400,
+  //   delay: 3000,
+  //  trailWidth: 1.72,
+  //  svgStyle: {
+  //    width: "100%",
+  //    height: "100%",
+  //  },
+  //  step: (state, bar) => {
+  //     bar.setText(Math.round(bar.value() * 100) + " %");
+  //   },
+  //  });
 
-  bar.animate(0.75);
+  //  bar.animate(0.75);
 
   // Contact form
   $(".art-input").keyup(function () {
